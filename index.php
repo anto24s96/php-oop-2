ce2d1de72ef9ad49e096930709ecd513e0d02de1
anto24s96 <anto24s96@gmail.com>
2024-01-31 16:07:49 +0100

    created Product class

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php 
+    /* DEFINISCO LA CLASSE PRODOTTO */
+    class Product
+    {
+        public $name;
+        public $image;
+        public $price;
+        public $description;
+
+        public function __construct($name, $image, $price, $description)
+        {
+            $this->name = $_name;
+            $this->image = $_image;
+            $this->price = $_price;
+            $this->description = $_description;
+        }
+    }
+?>
