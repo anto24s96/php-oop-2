f46aa142470bc765b6e71275ef9346f3d48f02c8
anto24s96 <anto24s96@gmail.com>
2024-01-31 17:25:47 +0100

    [UPDATE] created products elements

--- index.php
+++ index.php
@@ -70,5 +70,19 @@
         }
     }
 
+    /* DEFINISCO LE ISTANZE DELLA CLASSE CATEGORIA */
+    $dog = new Category('Cane');
+    $cat = new Category('Gatto');
+    $bird = new Category('Uccelli');
+    $fish = new Category('Pesci');
 
+    /* DEFINISCO LE ISTANZE DEI PRODOTTI */
+    $product_1 = new Food('Royal Canin','Mini Adult','https://arcaplanet.vtexassets.com/arquivos/ids/284621/Mini-Adult.jpg?v=638182891693570000', 40.99, 'Alimento completo per cani adulti di piccola taglia (fino a 10kg) - Oltre 10 mesi di età', $dog, 'Crocchette secche');
+    $product_2 = new Food('Almo Nature','Holistic Medium Adult con Pollo','https://arcaplanet.vtexassets.com/arquivos/ids/245173/almo-nature-holistic-cane-adult-medium-pollo-e-riso.jpg', 35.90,'Contiene carne o pesce freschi per un alta appetibilità.', $dog, 'Crocchette');
+    $product_3 = new Food('Almo Nature','Cat Daily Lattina','https://arcaplanet.vtexassets.com/arquivos/ids/245336/almo-daily-menu-cat-400-gr-vitello.jpg', 4.99,'Alimento umido in lattina, per gatti adulti.', $cat, 'Umido');
+    $prodcut_4 = new Food('Tetra','Mangime per Pesci Guppy in Fiocchi','https://arcaplanet.vtexassets.com/arquivos/ids/272714/tetra-guppy-mini-flakes.jpg', 2.99,'Fiocchi appositamente formulati per soddisfare le esigenze nutrizionali dei pesci guppy e altri pesci vivipari.', $fish,);
+    $product_5 = new Accessories('Ferplast','Voliera Wilma','https://arcaplanet.vtexassets.com/arquivos/ids/258384/voliera-wilma1.jpg',389.99,'Una raffinata e spaziosa voliera per canarini.', $bird);
+    $product_6 = new Accessories('Tetra','Cartucce Filtranti per Filtro EasyCrystal','https://arcaplanet.vtexassets.com/arquivos/ids/272741/tetra-easycrystal-filterpack-250-300.jpg',7.99, "Per un'acqua cristallina e sana, innovative cartucce filtranti.", $fish);
+    $product_7 = new Toy('Kong','Classic','https://arcaplanet.vtexassets.com/arquivos/ids/256599/kong-classic1.jpg',3.99,'Salta e rimbalza di qua e di là, soddisfa il bisogno di masticare e di giocare del cane', $dog,'Plastica');
+    $product_8 = new Toy('Trixie','Topini di peluche','https://arcaplanet.vtexassets.com/arquivos/ids/223852/trixie-gatto-gioco-active-mouse-peluche.jpg', 2.49, 'Gioco per gatti Topini peluche, un grande classico per il divertimento di Micio!', $cat ,'Peluche');
 ?>
